a181c1118e00b7fb66e49282f62dcc755449db32
barrett4467 <barrett4467@gmail.com>
2020-03-02 13:00:12 -0700

    Added gif

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Train-Tracker
-Train-Tracker is a schedule application built using HTML and CSS that uses momentJS to show current train schedules. Users can input trains of their choosing without worring that the data will be lost. The application stores data for each train input in firebase. Be sure to check out the app at https://barrett4467.github.io/Train-Tracker/
+Train-Tracker is a schedule application built using HTML and CSS that uses momentJS to show current train schedules. Users can input trains of their choosing without worrying that the data will be lost. The application stores data for each train input in a firebase database. 
+
+![](TrainTracker.gif)
+
+Be sure to check out the app at https://barrett4467.github.io/Train-Tracker/
+
+
+## Technologies:
+    HTML
+    CSS
+    jQuery
+    MomentJS
+    Firebase
--- assets/logic.js
+++ assets/logic.js
@@ -1,185 +1,185 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAatTV_0ToLA8vNFCSt0vMF4lwxNBEUN38",
-    authDomain: "train-tracker-a1c13.firebaseapp.com",
-    databaseURL: "https://train-tracker-a1c13.firebaseio.com",
-    projectId: "train-tracker-a1c13",
-    storageBucket: "",
-    messagingSenderId: "770599817204",
-    appId: "1:770599817204:web:f66b4e68e008b5239933a1"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAatTV_0ToLA8vNFCSt0vMF4lwxNBEUN38",
+  authDomain: "train-tracker-a1c13.firebaseapp.com",
+  databaseURL: "https://train-tracker-a1c13.firebaseio.com",
+  projectId: "train-tracker-a1c13",
+  storageBucket: "",
+  messagingSenderId: "770599817204",
+  appId: "1:770599817204:web:f66b4e68e008b5239933a1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
-  var initialName = "Thomas"
-  var initialDestination = "The Island of Sodor"
-  
-  var trainName = initialName;
-  var destination = initialDestination;
-  var firstTrain = "03:30"
-  var frequency = "15";
-  
-  var table = $("#train-data");
-  var train;
+var database = firebase.database();
+var initialName = "Thomas"
+var initialDestination = "The Island of Sodor"
+
+var trainName = initialName;
+var destination = initialDestination;
+var firstTrain = "03:30"
+var frequency = "15";
+
+var table = $("#train-data");
+var train;
 
 
-  function validation (){
+function validation() {
 
-    $("#train-input").on("input", function() {
-        var input = $(this);
-        var train = input.val();
-        input.addClass("invalid");
+  $("#train-input").on("input", function () {
+    var input = $(this);
+    var train = input.val();
+    input.addClass("invalid");
 
-        if (train){
-            input.removeClass("invalid").addClass("valid");
-            $(".error").remove();
-            return true;
+    if (train) {
+      input.removeClass("invalid").addClass("valid");
+      $(".error").remove();
+      return true;
 
-        } else {
-            input.removeClass("valid").addClass("invalid");
-            $(".error").text("Please enter a valid answer");
-            return false;
-        }
+    } else {
+      input.removeClass("valid").addClass("invalid");
+      $(".error").text("Please enter a valid answer");
+      return false;
+    }
 
-    })
-    $("#destination-input").on("input", function() {
-        var input = $(this);
-        var destination = input.val();
+  })
+  $("#destination-input").on("input", function () {
+    var input = $(this);
+    var destination = input.val();
 
-        if (destination){
-            input.removeClass("invalid").addClass("valid");
-            $(".error").remove();
-            return true;
-        } else {
-            input.removeClass("valid").addClass("invalid");
-            $(".error").text("Please enter a valid answer");
-            return false;
-        }
+    if (destination) {
+      input.removeClass("invalid").addClass("valid");
+      $(".error").remove();
+      return true;
+    } else {
+      input.removeClass("valid").addClass("invalid");
+      $(".error").text("Please enter a valid answer");
+      return false;
+    }
 
-    })
-    $("#time-input").on("input", function() {
-        var input = $(this);
-        var time = input.val();
-      console.log("Time: " + time);
-      
-        if (time){
-            input.removeClass("invalid").addClass("valid");
-            $(".error").remove();
-            return true;
+  })
+  $("#time-input").on("input", function () {
+    var input = $(this);
+    var time = input.val();
+    console.log("Time: " + time);
 
-        } else {
-            input.removeClass("valid").addClass("invalid");
-            $(".error").text("Please enter a valid answer");
-            return false;
-        };
+    if (time) {
+      input.removeClass("invalid").addClass("valid");
+      $(".error").remove();
+      return true;
 
-    })
-    $("#frequency-input").on("input", function() {
-      var input = $(this);
-      var frequency = input.val();
+    } else {
+      input.removeClass("valid").addClass("invalid");
+      $(".error").text("Please enter a valid answer");
+      return false;
+    };
 
-      if (frequency){
-          input.removeClass("invalid").addClass("valid");
-          $(".error").remove();
-          return true;
+  })
+  $("#frequency-input").on("input", function () {
+    var input = $(this);
+    var frequency = input.val();
 
-      } else {
-          input.removeClass("valid").addClass("invalid");
-          $(".error").text("Please enter a valid answer");
-          return false;
-      };
+    if (frequency) {
+      input.removeClass("invalid").addClass("valid");
+      $(".error").remove();
+      return true;
+
+    } else {
+      input.removeClass("valid").addClass("invalid");
+      $(".error").text("Please enter a valid answer");
+      return false;
+    };
 
   })
 }
 validation();
 
 
-$("#submit").on("click", function (event){
-    event.preventDefault();
-  
-    trainName = $("#train-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTrain = $("#time-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+
+  trainName = $("#train-input").val().trim();
+  destination = $("#destination-input").val().trim();
+  firstTrain = $("#time-input").val().trim();
+  frequency = $("#frequency-input").val().trim();
 
 
-//validates that all inputs have been applied 
-    if ($("#train-input").hasClass("valid")  && $("#destination-input").hasClass("valid") && $("#time-input").hasClass("valid") && $("#frequency-input").hasClass("valid")){
+  //validates that all inputs have been applied 
+  if ($("#train-input").hasClass("valid") && $("#destination-input").hasClass("valid") && $("#time-input").hasClass("valid") && $("#frequency-input").hasClass("valid")) {
 
-      database.ref().push({
-          trainName,
-          destination,
-          firstTrain,
-          frequency
-        });
-  
-        alert("Train has been added!");
-  
-        $("#train-input").val("");
-        $("#destination-input").val("");
-        $("#time-input").val("");
-        $("#frequency-input").val("");
-    } else {
-      alert("Please make a valid selection");
-    }
+    database.ref().push({
+      trainName,
+      destination,
+      firstTrain,
+      frequency
+    });
+
+    alert("Train has been added!");
+
+    $("#train-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+  } else {
+    alert("Please make a valid selection");
+  }
 
 })
 
 
-    database.ref().on("child_added", function(snapshot) {
-        console.log(snapshot.val());
+database.ref().on("child_added", function (snapshot) {
+  console.log(snapshot.val());
 
 
-        var trainName = snapshot.val().trainName;
-        var destination = snapshot.val().destination;
-        var firstTrain = snapshot.val().firstTrain;
-        var frequency = snapshot.val().frequency;
+  var trainName = snapshot.val().trainName;
+  var destination = snapshot.val().destination;
+  var firstTrain = snapshot.val().firstTrain;
+  var frequency = snapshot.val().frequency;
 
-        console.log(trainName, destination, firstTrain, frequency);
+  console.log(trainName, destination, firstTrain, frequency);
 
-        var firstTrainConverted = moment(firstTrain, "HH: mm").subtract(1, "years"); 
-        var difference = moment().diff(moment(firstTrainConverted), "minutes"); 
-        var remainder = difference % frequency;
-        var minutesTillTrain = frequency - remainder;
-        var nextTrainTime = moment().add(minutesTillTrain, "minutes");
-        var timeRemaining = minutesTillTrain;
-        var nextTrain = moment(nextTrainTime).format("h:mm");
-      $("#train-data > tbody").append(`<tr><td>${trainName}</td><td>${destination}</td><td>${"Every " + frequency + " minutes"}</td><td>${nextTrain}</td><td>${timeRemaining}</td><tr>`);
+  var firstTrainConverted = moment(firstTrain, "HH: mm").subtract(1, "years");
+  var difference = moment().diff(moment(firstTrainConverted), "minutes");
+  var remainder = difference % frequency;
+  var minutesTillTrain = frequency - remainder;
+  var nextTrainTime = moment().add(minutesTillTrain, "minutes");
+  var timeRemaining = minutesTillTrain;
+  var nextTrain = moment(nextTrainTime).format("h:mm");
+  $("#train-data > tbody").append(`<tr><td>${trainName}</td><td>${destination}</td><td>${"Every " + frequency + " minutes"}</td><td>${nextTrain}</td><td>${timeRemaining}</td><tr>`);
 
-      var children = database.ref();
+  var children = database.ref();
 
-      children.on("value", function(snapshot){
-        var kids = snapshot.numChildren();
-        console.log(kids);
-        
+  children.on("value", function (snapshot) {
+    var kids = snapshot.numChildren();
+    console.log(kids);
 
-        for (i = 0; i <= kids; i++){
-          $(".delete").attr("data-train", i);
-      
-        }
-        
-      })
-      
-      
-     
-    }, function(errorObject) {
-        console.log("The read Failed: " + errorObject.code);
 
-    });
+    for (i = 0; i <= kids; i++) {
+      $(".delete").attr("data-train", i);
+
+    }
+
+  })
 
 
 
-    $("#train-data").on("click", ".delete", function () {
-      var index = $(this).attr("data-train"); 
-      console.log("Index: " + index);
-      
+}, function (errorObject) {
+  console.log("The read Failed: " + errorObject.code);
 
-      })
-      // toDos.splice(index, 1);
+});
 
-      var rowCount = $('#train-data tr').length;
 
- 
+
+$("#train-data").on("click", ".delete", function () {
+  var index = $(this).attr("data-train");
+  console.log("Index: " + index);
+
+
+})
+// toDos.splice(index, 1);
+
+var rowCount = $('#train-data tr').length;
+
+
 
 
 
@@ -195,9 +195,9 @@ $("#submit").on("click", function (event){
 //       var roleData = $("<td>").text(role);
 //       var startDateData = $("<td>").text(startDate);
 //       var monthlyRateData = $("<td>").text(monthlyRate);
-      
+
 //       row.append(nameData);
 //       table.append(row);
 //     }
-    
+
 //   }
